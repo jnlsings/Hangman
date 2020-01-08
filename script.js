fc4e4478df3de47624ae02af33b6f2239994a23f
jnlsings <jnlsings@gmail.com>
2020-01-07 20:27:47 -0600

    Added button + troubleshooting
    Added random word button and attempted to separate function to generate word and function to guess letter.  Did get the input.value/index comparison to work. Added some CSS to facilitate troubleshooting.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const wordBank = [
   'countertenor',
   'contralto',
   'heldentenor',
-  'mezzo soprano'
+  'mezzo'
 ];
 
 const guess = document.querySelector('.letter-guess');
@@ -15,22 +15,50 @@ const guessBtn = document.querySelector('.guess-button');
 
 const answer = document.querySelector('.answer');
 
-guessBtn.addEventListener('click', handleGuessBtn);
+const input = document.querySelector('.letter-guess');
 
-function handleGuessBtn(event) {
+const guessedLetters = document.querySelector('.guessed-letters');
+
+const randomBtn = document.querySelector('.random-word-button');
+
+randomBtn.addEventListener('click', handleRandomBtn);
+
+// guessBtn.addEventListener('click', handleGuessBtn);
+
+// create function that when random button is pressed, random word is generated from wordBank and stored as an array of characters each in its own div, but hidden
+function handleRandomBtn(event) {
   event.preventDefault();
+  let word = wordBank[Math.floor(Math.random() * wordBank.length)];
+  console.log(word);
+
+  //   function handleGuessBtn(event) {
   for (let i = 0; i < 1; i += 1) {
-    let word = wordBank[Math.floor(Math.random() * wordBank.length)];
-    console.log(word);
     let letters = word.split('');
     console.log(letters);
+
     for (let i = 0; i < letters.length; i++) {
-      // store each character in a box
-      let space = document.createElement('div');
-      space.setAttribute('class', 'letter-holder');
-      space.append(letters[i]);
-      console.log(space);
-      answer.appendChild(space);
+      // create div + store each character in one
+
+      let letterSpace = document.createElement('div');
+      letterSpace.setAttribute('class', 'letter-holder');
+      letterSpace.append(letters[i]);
+      // attach character divs to answer
+      answer.appendChild(letterSpace);
+      letterSpace.style.display = 'inline';
+      //   letterSpace.hide();
+
+      if (letterSpace.innerText === input.value) {
+        letterSpace.style.backgroundColor = 'white';
+        // change display of that letter only to block
+        // letters[i].style.visibility = 'visible';
+      } else if (letterSpace.innerText !== input.value) {
+        console.log('Try Again');
+      }
     }
+    guessedLetters.append(input.value);
+
+    // function handleGuessBtn(event) {
+    //   event.preventDefault();
   }
 }
+// }
